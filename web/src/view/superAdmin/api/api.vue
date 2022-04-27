042db0197673244a846df189bfb7040d7c8c19e6
jianghao <jianghao@haplox.com>
2022-04-27 11:32:28 +0800

    更新api

--- web/src/view/superAdmin/api/api.vue
+++ web/src/view/superAdmin/api/api.vue
@@ -22,39 +22,82 @@
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button size="small" type="primary" icon="search" @click="onSubmit">查询</el-button>
+          <el-button size="small" type="primary" icon="search" @click="onSubmit">
+            查询
+          </el-button>
           <el-button size="small" icon="refresh" @click="onReset">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
     <div class="gva-table-box">
       <div class="gva-btn-list">
-        <el-button size="small" type="primary" icon="plus" @click="openDialog('addApi')">新增</el-button>
+        <el-button size="small" type="primary" icon="plus" @click="openDialog('addApi')">
+          新增
+        </el-button>
         <el-popover v-model:visible="deleteVisible" placement="top" width="160">
           <p>确定要删除吗？</p>
-          <div style="text-align: right; margin-top: 8px;">
-            <el-button size="small" type="text" @click="deleteVisible = false">取消</el-button>
+          <div style="text-align: right; margin-top: 8px">
+            <el-button size="small" type="text" @click="deleteVisible = false">
+              取消
+            </el-button>
             <el-button size="small" type="primary" @click="onDelete">确定</el-button>
           </div>
           <template #reference>
-            <el-button icon="delete" size="small" :disabled="!apis.length" style="margin-left: 10px;" @click="deleteVisible = true">删除</el-button>
+            <el-button
+              icon="delete"
+              size="small"
+              :disabled="!apis.length"
+              style="margin-left: 10px"
+              @click="deleteVisible = true"
+            >
+              删除
+            </el-button>
           </template>
         </el-popover>
       </div>
-      <el-table :data="tableData" @sort-change="sortChange" @selection-change="handleSelectionChange">
+      <el-table
+        :data="tableData"
+        @sort-change="sortChange"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55" />
         <el-table-column
-          type="selection"
-          width="55"
+          align="left"
+          label="id"
+          min-width="60"
+          prop="ID"
+          sortable="custom"
         />
-        <el-table-column align="left" label="id" min-width="60" prop="ID" sortable="custom" />
-        <el-table-column align="left" label="API路径" min-width="150" prop="path" sortable="custom" />
-        <el-table-column align="left" label="API分组" min-width="150" prop="apiGroup" sortable="custom" />
-        <el-table-column align="left" label="API简介" min-width="150" prop="description" sortable="custom" />
-        <el-table-column align="left" label="请求" min-width="150" prop="method" sortable="custom">
+        <el-table-column
+          align="left"
+          label="API路径"
+          min-width="150"
+          prop="path"
+          sortable="custom"
+        />
+        <el-table-column
+          align="left"
+          label="API分组"
+          min-width="150"
+          prop="apiGroup"
+          sortable="custom"
+        />
+        <el-table-column
+          align="left"
+          label="API简介"
+          min-width="150"
+          prop="description"
+          sortable="custom"
+        />
+        <el-table-column
+          align="left"
+          label="请求"
+          min-width="150"
+          prop="method"
+          sortable="custom"
+        >
           <template #default="scope">
-            <div>
-              {{ scope.row.method }} / {{ methodFiletr(scope.row.method) }}
-            </div>
+            <div>{{ scope.row.method }} / {{ methodFiletr(scope.row.method) }}</div>
           </template>
         </el-table-column>
 
@@ -65,13 +108,15 @@
               size="small"
               type="text"
               @click="editApiFunc(scope.row)"
-            >编辑</el-button>
+              >编辑</el-button
+            >
             <el-button
               icon="delete"
               size="small"
               type="text"
               @click="deleteApiFunc(scope.row)"
-            >删除</el-button>
+              >删除</el-button
+            >
           </template>
         </el-table-column>
       </el-table>
@@ -86,17 +131,20 @@
           @size-change="handleSizeChange"
         />
       </div>
-
     </div>
 
-    <el-dialog v-model="dialogFormVisible" :before-close="closeDialog" :title="dialogTitle">
+    <el-dialog
+      v-model="dialogFormVisible"
+      :before-close="closeDialog"
+      :title="dialogTitle"
+    >
       <warning-bar title="新增API，需要在角色管理内配置权限才可使用" />
       <el-form ref="apiForm" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="路径" prop="path">
           <el-input v-model="form.path" autocomplete="off" />
         </el-form-item>
         <el-form-item label="请求" prop="method">
-          <el-select v-model="form.method" placeholder="请选择" style="width:100%">
+          <el-select v-model="form.method" placeholder="请选择" style="width: 100%">
             <el-option
               v-for="item in methodOptions"
               :key="item.value"
@@ -124,8 +172,8 @@
 
 <script>
 export default {
-  name: 'Api',
-}
+  name: "Api",
+};
 </script>
 
 <script setup>
@@ -135,245 +183,241 @@ import {
   createApi,
   updateApi,
   deleteApi,
-  deleteApisByIds
-} from '@/api/api'
-import { toSQLLine } from '@/utils/stringFun'
-import warningBar from '@/components/warningBar/warningBar.vue'
-import { ref } from 'vue'
-import { ElMessage, ElMessageBox } from 'element-plus'
+  deleteApisByIds,
+} from "@/api/api";
+import { toSQLLine } from "@/utils/stringFun";
+import warningBar from "@/components/warningBar/warningBar.vue";
+import { ref } from "vue";
+import { ElMessage, ElMessageBox } from "element-plus";
 
 const methodFiletr = (value) => {
-  const target = methodOptions.value.filter(item => item.value === value)[0]
-  return target && `${target.label}`
-}
+  const target = methodOptions.value.filter((item) => item.value === value)[0];
+  return target && `${target.label}`;
+};
 
-const apis = ref([])
+const apis = ref([]);
 const form = ref({
-  path: '',
-  apiGroup: '',
-  method: '',
-  description: ''
-})
+  path: "",
+  apiGroup: "",
+  method: "",
+  description: "",
+});
 const methodOptions = ref([
   {
-    value: 'POST',
-    label: '创建',
-    type: 'success'
+    value: "POST",
+    label: "创建",
+    type: "success",
   },
   {
-    value: 'GET',
-    label: '查看',
-    type: ''
+    value: "GET",
+    label: "查看",
+    type: "",
   },
   {
-    value: 'PUT',
-    label: '更新',
-    type: 'warning'
+    value: "PUT",
+    label: "更新",
+    type: "warning",
   },
   {
-    value: 'DELETE',
-    label: '删除',
-    type: 'danger'
-  }
-])
+    value: "DELETE",
+    label: "删除",
+    type: "danger",
+  },
+]);
 
-const type = ref('')
+const type = ref("");
 const rules = ref({
-  path: [{ required: true, message: '请输入api路径', trigger: 'blur' }],
-  apiGroup: [
-    { required: true, message: '请输入组名称', trigger: 'blur' }
-  ],
-  method: [
-    { required: true, message: '请选择请求方式', trigger: 'blur' }
-  ],
-  description: [
-    { required: true, message: '请输入api介绍', trigger: 'blur' }
-  ]
-})
+  path: [{ required: true, message: "请输入api路径", trigger: "blur" }],
+  apiGroup: [{ required: true, message: "请输入组名称", trigger: "blur" }],
+  method: [{ required: true, message: "请选择请求方式", trigger: "blur" }],
+  description: [{ required: true, message: "请输入api介绍", trigger: "blur" }],
+});
 
-const page = ref(1)
-const total = ref(0)
-const pageSize = ref(10)
-const tableData = ref([])
-const searchInfo = ref({})
+const page = ref(1);
+const total = ref(0);
+const pageSize = ref(10);
+const tableData = ref([]);
+const searchInfo = ref({});
 
 const onReset = () => {
-  searchInfo.value = {}
-}
+  searchInfo.value = {};
+};
 // 搜索
 
 const onSubmit = () => {
-  page.value = 1
-  pageSize.value = 10
-  getTableData()
-}
+  page.value = 1;
+  pageSize.value = 10;
+  getTableData();
+};
 
 // 分页
 const handleSizeChange = (val) => {
-  pageSize.value = val
-  getTableData()
-}
+  pageSize.value = val;
+  getTableData();
+};
 
 const handleCurrentChange = (val) => {
-  page.value = val
-  getTableData()
-}
+  page.value = val;
+  getTableData();
+};
 
 // 排序
 const sortChange = ({ prop, order }) => {
   if (prop) {
-    if (prop === 'ID') {
-      prop = 'id'
+    if (prop === "ID") {
+      prop = "id";
     }
-    searchInfo.value.orderKey = toSQLLine(prop)
-    searchInfo.value.desc = order === 'descending'
+    searchInfo.value.orderKey = toSQLLine(prop);
+    searchInfo.value.desc = order === "descending";
   }
-  getTableData()
-}
+  getTableData();
+};
 
 // 查询
-const getTableData = async() => {
-  const table = await getApiList({ page: page.value, pageSize: pageSize.value, ...searchInfo.value })
+const getTableData = async () => {
+  const table = await getApiList({
+    page: page.value,
+    pageSize: pageSize.value,
+    ...searchInfo.value,
+  });
   if (table.code === 0) {
-    tableData.value = table.data.list
-    total.value = table.data.total
-    page.value = table.data.page
-    pageSize.value = table.data.pageSize
+    tableData.value = table.data.list;
+    total.value = table.data.total;
+    page.value = table.data.page;
+    pageSize.value = table.data.pageSize;
   }
-}
+};
 
-getTableData()
+getTableData();
 
 // 批量操作
 const handleSelectionChange = (val) => {
-  apis.value = val
-}
+  apis.value = val;
+};
 
-const deleteVisible = ref(false)
-const onDelete = async() => {
-  const ids = apis.value.map(item => item.ID)
-  const res = await deleteApisByIds({ ids })
+const deleteVisible = ref(false);
+const onDelete = async () => {
+  const ids = apis.value.map((item) => item.ID);
+  const res = await deleteApisByIds({ ids });
   if (res.code === 0) {
     ElMessage({
-      type: 'success',
-      message: res.msg
-    })
+      type: "success",
+      message: res.msg,
+    });
     if (tableData.value.length === ids.length && page.value > 1) {
-      page.value--
+      page.value--;
     }
-    deleteVisible.value = false
-    getTableData()
+    deleteVisible.value = false;
+    getTableData();
   }
-}
+};
 
 // 弹窗相关
-const apiForm = ref(null)
+const apiForm = ref(null);
 const initForm = () => {
-  apiForm.value.resetFields()
+  apiForm.value.resetFields();
   form.value = {
-    path: '',
-    apiGroup: '',
-    method: '',
-    description: ''
-  }
-}
+    path: "",
+    apiGroup: "",
+    method: "",
+    description: "",
+  };
+};
 
-const dialogTitle = ref('新增Api')
-const dialogFormVisible = ref(false)
+const dialogTitle = ref("新增Api");
+const dialogFormVisible = ref(false);
 const openDialog = (key) => {
   switch (key) {
-    case 'addApi':
-      dialogTitle.value = '新增Api'
-      break
-    case 'edit':
-      dialogTitle.value = '编辑Api'
-      break
+    case "addApi":
+      dialogTitle.value = "新增Api";
+      break;
+    case "edit":
+      dialogTitle.value = "编辑Api";
+      break;
     default:
-      break
+      break;
   }
-  type.value = key
-  dialogFormVisible.value = true
-}
+  type.value = key;
+  dialogFormVisible.value = true;
+};
 const closeDialog = () => {
-  initForm()
-  dialogFormVisible.value = false
-}
+  initForm();
+  dialogFormVisible.value = false;
+};
 
-const editApiFunc = async(row) => {
-  const res = await getApiById({ id: row.ID })
-  form.value = res.data.api
-  openDialog('edit')
-}
+const editApiFunc = async (row) => {
+  const res = await getApiById({ id: row.ID });
+  form.value = res.data.api;
+  openDialog("edit");
+};
 
-const enterDialog = async() => {
-  apiForm.value.validate(async valid => {
+const enterDialog = async () => {
+  apiForm.value.validate(async (valid) => {
     if (valid) {
       switch (type.value) {
-        case 'addApi':
+        case "addApi":
           {
-            const res = await createApi(form.value)
+            const res = await createApi(form.value);
             if (res.code === 0) {
               ElMessage({
-                type: 'success',
-                message: '添加成功',
-                showClose: true
-              })
+                type: "success",
+                message: "添加成功",
+                showClose: true,
+              });
             }
-            getTableData()
-            closeDialog()
+            getTableData();
+            closeDialog();
           }
 
-          break
-        case 'edit':
+          break;
+        case "edit":
           {
-            const res = await updateApi(form.value)
+            const res = await updateApi(form.value);
             if (res.code === 0) {
               ElMessage({
-                type: 'success',
-                message: '编辑成功',
-                showClose: true
-              })
+                type: "success",
+                message: "编辑成功",
+                showClose: true,
+              });
             }
-            getTableData()
-            closeDialog()
+            getTableData();
+            closeDialog();
           }
-          break
+          break;
         default:
           // eslint-disable-next-line no-lone-blocks
           {
             ElMessage({
-              type: 'error',
-              message: '未知操作',
-              showClose: true
-            })
+              type: "error",
+              message: "未知操作",
+              showClose: true,
+            });
           }
-          break
+          break;
       }
     }
-  })
-}
+  });
+};
 
-const deleteApiFunc = async(row) => {
-  ElMessageBox.confirm('此操作将永久删除所有角色下该api, 是否继续?', '提示', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  })
-    .then(async() => {
-      const res = await deleteApi(row)
-      if (res.code === 0) {
-        ElMessage({
-          type: 'success',
-          message: '删除成功!'
-        })
-        if (tableData.value.length === 1 && page.value > 1) {
-          page.value--
-        }
-        getTableData()
+const deleteApiFunc = async (row) => {
+  ElMessageBox.confirm("此操作将永久删除所有角色下该api, 是否继续?", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  }).then(async () => {
+    const res = await deleteApi(row);
+    if (res.code === 0) {
+      ElMessage({
+        type: "success",
+        message: "删除成功!",
+      });
+      if (tableData.value.length === 1 && page.value > 1) {
+        page.value--;
       }
-    })
-}
-
+      getTableData();
+    }
+  });
+};
 </script>
 
 <style scoped lang="scss">
