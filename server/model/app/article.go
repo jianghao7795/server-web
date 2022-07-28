838ff3ecfd41995e8b383095feeb15fe2ab4781a
jianghao <jianghao@haplox.com>
2022-07-28 19:32:55 +0800

    fix: 文章的标签 article server 服务

--- server/model/app/article.go
+++ server/model/app/article.go
@@ -7,10 +7,11 @@ type Article struct {
 	TagId      int    `json:"tag_id" form:"tag_id" gorm:"column:tag_id;comment:标签ID"`
 	Title      string `json:"title" form:"title" gorm:"column:title;comment:文章标题;size:191;"`
 	Desc       string `json:"desc" form:"desc" gorm:"column:desc;comment:文章简述;"`
-	Context    string `json:"context" form:"context" gorm:"column:context;comment:文章内容;"`
-	State      string `json:"state" form:"state" gorm:"column:state;comment:文章状态;"`
+	Content    string `json:"content" form:"content" gorm:"column:content;comment:文章内容;"`
+	State      int    `json:"state" form:"state" gorm:"column:state;comment:文章状态;"`
 	ModifiedBy string `json:"modified_by" form:"modified_by" gorm:"column:modified_by;comment:修改人;"`
 	CreatedBy  string `json:"created_by" form:"created_by" gorm:"column:created_by;comment:创建人;"`
+	Tag        AppTab `json:"tag" form:"tag" gorm:"foreignKey:TagId"`
 }
 
 // 表名
---
+++ server/service/app/article.go
@@ -0,0 +1,66 @@
+package app
+
+import (
+	"server/global"
+	"server/model/app"
+	appReq "server/model/app/request"
+	"server/model/common/request"
+)
+
+type ArticleService struct{}
+
+// create
+func (articleSearch *ArticleService) CreateArticle(article app.Article) (err error) {
+	err = global.GVA_DB.Create(&article).Error
+	return
+}
+
+// delete
+func (articleSearch *ArticleService) DeleteArticle(article app.Article) (err error) {
+	err = global.GVA_DB.Delete(&article).Error
+	return
+}
+
+// delete by ids
+func (articleSearch *ArticleService) DeleteArticleByIds(ids request.IdsReq) (err error) {
+	var articleIds *[]app.Article = &[]app.Article{}
+	err = global.GVA_DB.Delete(articleIds, "id in ?", ids.Ids).Error
+	return
+}
+
+// update
+func (articleSearch *ArticleService) UpdateArticle(article app.Article) (err error) {
+	var articleDetail app.Article
+	err = global.GVA_DB.Where("id = ?", article.ID).First(&articleDetail).Error
+	if err != nil {
+		return
+	}
+	err = global.GVA_DB.Save(&article).Error
+	return err
+}
+
+// getDetail by id
+func (articleSearch *ArticleService) GetArticle(id uint) (article app.Article, err error) {
+	err = global.GVA_DB.Where("id = ?", id).First(&article).Error
+	return
+}
+
+// getList
+
+func (articleSearch *ArticleService) GetArticleInfoList(info appReq.ArticleSearch) (list interface{}, total int64, err error) {
+	limit := info.PageSize
+	offset := info.PageSize * (info.Page - 1)
+	db := global.GVA_DB.Model(&app.Article{}).Preload("Tag")
+	var articles []app.Article
+
+	if info.Title != "" {
+		db = db.Where("title = ?", info.Title)
+	}
+
+	err = db.Count(&total).Error
+	if err != nil {
+		return
+	}
+	err = db.Limit(limit).Offset(offset).Order("id desc").Find(&articles).Error
+	return articles, total, err
+}
