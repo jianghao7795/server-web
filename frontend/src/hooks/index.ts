1890fae47e2036367ddbd3f290da0998b275b149
jianghao <jianghao@haplox.com>
2022-08-11 08:48:39 +0800

    feat: 增加简单的hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,3 @@
+import useLoading from './loading/useLoading';
+
+export { useLoading };
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
 import TheWelcome from '@/components/TheWelcome.vue';
-import { NInput, NButton, NIcon } from 'naive-ui';
+import { NInputNumber, NButton, NIcon } from 'naive-ui';
+// import { ref } from 'vue';
 import { AddFilled } from '@vicons/material';
 import { useCounterStore } from '@/stores/counter';
+import { useLoading } from '@/hooks';
 
 const counterStore = useCounterStore();
+// const loadingRef = ref<Boolean>(false);
+const { loading, startLoading, endLoading } = useLoading();
+const handlAdd = () => {
+  startLoading();
+  setTimeout(() => {
+    counterStore.increment();
+    endLoading();
+  }, 1000);
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <n-input placeholder="请输入" />
+    <n-input-number placeholder="请输入" v-model:value="counterStore.counter" />
     <p>{{ counterStore.counter }}</p>
     <p>{{ counterStore.doubleCount }}</p>
-    <n-button v-on:click="counterStore.increment">
+    <n-button :loading="loading" type="primary" v-on:click="handlAdd">
       <template #icon>
-        <AddFilled />
+        <n-icon>
+          <AddFilled />
+        </n-icon>
       </template>
       加
     </n-button>
