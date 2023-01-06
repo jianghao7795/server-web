541cc03e67e48ce36ded6ea44ab639017d5bef67
jianghao <jianghao@haplox.com>
2023-01-06 09:37:51 +0800

    feat: 修复基本信息的css

--- web/src/view/article/baseMessage/baseMessage.vue
+++ web/src/view/article/baseMessage/baseMessage.vue
@@ -189,38 +189,9 @@ const handleSubmit = () => {
 </script>
 
 <style scoped lang="scss">
-// .avatar-uploader .el-upload {
-//   border: 1px dashed var(--el-border-color);
-//   border-radius: 6px;
-//   cursor: pointer;
-//   position: relative;
-//   overflow: hidden;
-//   transition: var(--el-transition-duration-fast);
-// }
-
-// .avatar-uploader .el-upload:hover {
-//   border-color: var(--el-color-primary);
-// }
-
-// .el-icon.avatar-uploader-icon {
-//   font-size: 28px;
-//   color: #8c939d;
-//   width: 178px;
-//   height: 178px;
-//   text-align: center;
-// }
-
-// .card-header {
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-// }
-
-// .text {
-//   font-size: 14px;
-// }
-
-// .item {
-//   margin-bottom: 18px;
-// }
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
