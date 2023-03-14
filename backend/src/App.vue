3055348c06e73a942d76ffa924affee0c9187579
jianghao <97561120@qq.com>
2023-03-14 21:30:54 +0800

    feat: 更新配置

--- backend/src/App.vue
+++ backend/src/App.vue
@@ -23,12 +23,14 @@ import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 @import "@/style/main.scss";
 @import "@/style/base.scss";
 @import "@/style/mobile.scss";
+
 #app {
   background: #eee;
   height: 100vh;
   overflow: hidden;
   font-weight: 400 !important;
 }
+
 .el-button {
   font-weight: 400 !important;
 }
@@ -42,6 +44,7 @@ a {
   transition: color 0.3s;
   -webkit-text-decoration-skip: objects;
 }
+
 // 加载条的 css
 #nprogress .bar {
   background-color: #409eff;
