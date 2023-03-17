adb809d76baa9aae665e659ce61c127110e11b80
jianghao <jianghao@haplox.com>
2023-03-17 17:07:18 +0800

    feat: frontend 评论

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -9,6 +9,8 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    AAvatar: typeof import('ant-design-vue/es')['Avatar']
+    AComment: typeof import('ant-design-vue/es')['Comment']
     Comments: typeof import('./src/components/comments/index.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- frontend/src/views/article/detail.vue
+++ frontend/src/views/article/detail.vue
@@ -16,6 +16,78 @@
       <MdEditor :style="{ width: '100%' }" :model-value="articleStore.detail?.content" :theme="theme ? 'dark' : 'light'"
         :pageFullscreen="true" :previewOnly="true"></MdEditor>
     </div>
+    <n-input placeholder="评论" type="textarea" size="small" :autosize="{
+      minRows: 3,
+      maxRows: 5
+    }" />
+    <a-comment>
+      <template #actions>
+        <span key="comment-nested-reply-to">Reply to</span>
+      </template>
+      <template #author>
+        <a>Han Solo</a>
+      </template>
+      <template #avatar>
+        <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+      </template>
+      <template #content>
+        <p>
+          We supply a series of design principles, practical patterns and high quality design
+          resources (Sketch and Axure).
+        </p>
+      </template>
+      <a-comment>
+        <template #actions>
+          <span>Reply to</span>
+        </template>
+        <template #author>
+          <a>Han Solo</a>
+        </template>
+        <template #avatar>
+          <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+        </template>
+        <template #content>
+          <p>
+            We supply a series of design principles, practical patterns and high quality design
+            resources (Sketch and Axure).
+          </p>
+        </template>
+        <a-comment>
+          <template #actions>
+            <span>Reply to</span>
+          </template>
+          <template #author>
+            <a>Han Solo</a>
+          </template>
+          <template #avatar>
+            <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+          </template>
+          <template #content>
+            <p>
+              We supply a series of design principles, practical patterns and high quality design
+              resources (Sketch and Axure).
+            </p>
+          </template>
+        </a-comment>
+        <a-comment>
+          <template #actions>
+            <span>Reply to</span>
+          </template>
+          <template #author>
+            <a>Han Solo</a>
+          </template>
+          <template #avatar>
+            <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+          </template>
+          <template #content>
+            <p>
+              We supply a series of design principles, practical patterns and high quality design
+              resources (Sketch and Axure).
+            </p>
+          </template>
+        </a-comment>
+      </a-comment>
+    </a-comment>
   </div>
 </template>
 
