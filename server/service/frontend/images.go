f1e40d835bb019fa74685a415c8be30c31cd1286
jianghao <jianghao@haplox.com>
2023-03-29 11:59:20 +0800

    feat: 背景图片的获取更新条件

--- server/service/frontend/images.go
+++ server/service/frontend/images.go
@@ -8,6 +8,6 @@ import (
 type FrontendImages struct{}
 
 func (s *FrontendImages) GetImagesList() (list []frontend.ExaFile, err error) {
-	err = global.DB.Model(&frontend.ExaFile{}).Find(&list).Error
+	err = global.DB.Model(&frontend.ExaFile{}).Where("proportion > 1.6").Order("id desc").Find(&list).Error
 	return
 }
