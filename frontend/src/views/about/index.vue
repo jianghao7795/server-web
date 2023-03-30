6529addd426e9b1cc10f5f0638880d4129b017f9
jianghao <jianghao@haplox.com>
2023-03-30 15:44:03 +0800

    feat: frontend 关于我

--- frontend/src/views/about/index.vue
+++ frontend/src/views/about/index.vue
@@ -1,25 +1,11 @@
 <template>
   <div class="view-content">
     <h1 class="view-center">关于我</h1>
-    <div>
-      <Image />
-    </div>
-    <p>{{ aboutMeMessage?.aboutMe }}</p>
+    <p v-if="userStore.currentUser.user.ID !== 0">{{ userStore.currentUser.user.content }}</p>
   </div>
 </template>
 
 <script lang="ts" name="About" setup>
-import Image from "./Image.vue";
-import { aboutMe } from "@/services/about";
-import { onMounted, ref } from "vue";
-
-const aboutMeMessage = ref<API.AboutMe>();
-
-onMounted(async () => {
-  const resp = await aboutMe();
-  // console.log(resp);
-  if (resp?.code === 0) {
-    aboutMeMessage.value = resp.data;
-  }
-});
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
 </script>
