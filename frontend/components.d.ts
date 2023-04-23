52d22cb66c599c3b349dbebb0dd69e33be8297a6
jianghao <jianghao@haplox.com>
2023-04-23 11:31:32 +0800

    fix: 修复变异解析（新）的初始化

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -32,7 +32,6 @@ declare module '@vue/runtime-core' {
     NForm: typeof import('naive-ui')['NForm']
     NFormItem: typeof import('naive-ui')['NFormItem']
     NH4: typeof import('naive-ui')['NH4']
-    NIcon: typeof import('naive-ui')['NIcon']
     NInput: typeof import('naive-ui')['NInput']
     NInputGroup: typeof import('naive-ui')['NInputGroup']
     NLayout: typeof import('naive-ui')['NLayout']
@@ -54,7 +53,6 @@ declare module '@vue/runtime-core' {
     NTag: typeof import('naive-ui')['NTag']
     NThing: typeof import('naive-ui')['NThing']
     NUpload: typeof import('naive-ui')['NUpload']
-    Picker: typeof import('./src/components/Picker/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
