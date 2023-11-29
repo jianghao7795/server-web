22e5c4e607dc9f91a701d5ab4631a99c9aa50e71
jianghao <jianghao@haplox.com>
2023-11-29 15:03:21 +0800

    fix: server更新frontend API

--- server/api/v1/frontend/comment.go
+++ server/api/v1/frontend/comment.go
@@ -12,11 +12,16 @@ import (
 
 type CommentApi struct{}
 
+var commentService = frontendService.Comment
+
 func (s *CommentApi) GetCommentByArticleId(c *gin.Context) {
 	articleId := c.Param("articleId")
-	id, _ := strconv.Atoi(articleId)
-	// log.Println("artilceId", id)
-	if articleComment, err := frontendService.Comment.GetCommentByArticleId(id); err != nil {
+	id, err := strconv.Atoi(articleId)
+	if err != nil {
+		global.LOG.Error("获取articleId 失败!", zap.Error(err))
+		response.FailWithMessage("获取articleId 失败", c)
+	}
+	if articleComment, err := commentService.GetCommentByArticleId(id); err != nil {
 		global.LOG.Error("获取失败!", zap.Error(err))
 		response.FailWithMessage("获取失败", c)
 	} else {
@@ -27,7 +32,7 @@ func (s *CommentApi) GetCommentByArticleId(c *gin.Context) {
 func (s *CommentApi) CreatedComment(c *gin.Context) {
 	var comment frontend.Comment
 	_ = c.ShouldBindJSON(&comment)
-	if id, err := frontendService.Comment.CreatedComment(comment); err != nil {
+	if id, err := commentService.CreatedComment(comment); err != nil {
 		global.LOG.Error("创建失败!", zap.Error(err))
 		response.FailWithMessage("创建失败", c)
 	} else {
--- server/router/frontend/frontend.go
+++ server/router/frontend/frontend.go
@@ -20,20 +20,20 @@ func (s *FrontendRouter) InitFrontendRouter(Router *gin.RouterGroup) {
 	{
 		frontend.GET("getArticleList", frontendArticleApi.GetArticleList)
 		frontend.GET("getArticle/:id", frontendArticleApi.GetArticleDetail)
-		frontend.GET("/getSearchArticle/:name/:value", frontendArticleApi.GetSearchArticle)
+		frontend.GET("getSearchArticle/:name/:value", frontendArticleApi.GetSearchArticle)
 	}
 	var frontendCommentApi = v1.ApiGroupApp.FrontendApiGroup.CommentApi
 	{
-		frontend.GET("/getArticleComment/:articleId", frontendCommentApi.GetCommentByArticleId)
-		frontend.POST("/createdComment", middleware.OperationRecord(), frontendCommentApi.CreatedComment)
+		frontend.GET("getArticleComment/:articleId", frontendCommentApi.GetCommentByArticleId)
+		frontend.POST("createdComment", middleware.OperationRecord(), frontendCommentApi.CreatedComment)
 	}
 	var frontendUserApi = v1.ApiGroupApp.FrontendApiGroup.FrontendUser
 	{
-		frontend.GET("getImages", frontendUserApi.GetImages)
+		frontend.GET("getImages", middleware.JWTAuth(), frontendUserApi.GetImages)
 		frontend.POST("login", frontendUserApi.Login)
 		frontend.GET("getCurrentUser", middleware.JWTAuth(), frontendUserApi.GetCurrent)
-		frontend.PUT("updateBackgroundImage", middleware.OperationRecord(), frontendUserApi.UpdateUserBackgroudImage)
-		frontend.PUT("resetPassword", middleware.OperationRecord(), frontendUserApi.UpdatePassword)
+		frontend.PUT("updateBackgroundImage", middleware.JWTAuth(), middleware.OperationRecord(), frontendUserApi.UpdateUserBackgroudImage)
+		frontend.PUT("resetPassword", middleware.JWTAuth(), middleware.OperationRecord(), frontendUserApi.UpdatePassword)
 		frontend.POST("register", frontendUserApi.RegisterUser)
 		frontend.PUT("updateUser", middleware.JWTAuth(), middleware.OperationRecord(), frontendUserApi.UpdateUser)
 	}
